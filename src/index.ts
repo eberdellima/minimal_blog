dae4ee56616396338d39ae075e466b981e4d1646
Ergi <eberdellima@outlook.com>
2021-02-17 14:47:24 +0100

    setup router loader

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import "reflect-metadata";
 import * as dotenv from "dotenv";
 import { createConnection } from "typeorm";
 import express, { Request, Response } from "express";
+import { loadRouters } from "./loaders/router";
 
 dotenv.config();
 createConnection().then(async() => {
 
   const app = express();
+  loadRouters(app);
 
   app.get("/", (request: Request, response: Response) => {
     response.status(200).send(
