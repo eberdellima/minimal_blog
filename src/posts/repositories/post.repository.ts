251440851f0b6113bb1101c0ea903c3672f1b131
Bhfreagra <eberdellima@outlook.com>
2021-02-20 12:18:41 +0100

    Add endpoint for posts list

---
+++ src/posts/repositories/post.repository.ts
@@ -0,0 +1,25 @@
+import { EntityRepository, Repository } from "typeorm";
+import { IPaginationDTO } from "../../common/utilities/pagination.interface";
+import { Post } from "../models/post.entity";
+
+@EntityRepository(Post)
+export class PostRepository extends Repository<Post> {
+
+  public async getPosts(paginationDTO: IPaginationDTO) {
+    const qb = this.createQueryBuilder('p');
+    
+    if (paginationDTO.size) {
+      qb.take(paginationDTO.size);
+    }
+
+    if (paginationDTO.offset) {
+      qb.skip(paginationDTO.offset);
+    }
+
+    if (paginationDTO.orderBy) {
+      qb.orderBy(paginationDTO.orderBy, paginationDTO.orderDirection);
+    }
+
+    return qb.getManyAndCount();
+  }
+}
--- src/posts/router.ts
+++ src/posts/router.ts
@@ -1,6 +1,22 @@
 import { Router } from "express";
+import { getCustomRepository } from "typeorm";
+import { PaginationValidator } from "../common/middlewares/paginator.validator";
+import { PostController } from "./controllers/post.controller";
+import { PostManager } from "./services/post.manager";
+import { PostRepository } from './repositories/post.repository';
 
 
 export function configureRouter(router: Router) {
+
+  const paginationValidator = new PaginationValidator();
+
+  const postRepository = getCustomRepository(PostRepository);
+  const postManager = new PostManager(postRepository);
+  const postController = new PostController(postManager);
+
+  router.get('/', [
+    paginationValidator.validate(),
+    postController.listPosts,
+  ]);
   
 }
