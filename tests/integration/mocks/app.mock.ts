2a0e6a827a7585d9a32305d7e41e21f529bc2e9c
Bhfreagra <eberdellima@outlook.com>
2021-02-21 14:14:17 +0100

    modify test settings

--- tests/integration/mocks/app.mock.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { loadRouters } from '../../../src/loaders/router';
-import { loadServerConfiguration } from '../../../src/loaders/server';
-
-const app = express();
-loadServerConfiguration(app);
-loadRouters(app);
-
-export default app;
